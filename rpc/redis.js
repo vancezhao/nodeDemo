6b7b3849f0b808bc6a0331bab09ea5dbf9db1a84
Vance Zhao <zhaokai@nonobank.com>
2016-01-01 19:07:13 +0800

    Add kafka support

--- rpc/redis.js
+++ rpc/redis.js
@@ -27,6 +27,35 @@ var redis = new Redis(redis_port, redis_host);
 var pipeline = redis.pipeline();
 var future = pipeline.set('vance_java3', 'vance_java3').exec();
 
+
+//var kafka = require('kafka-node');
+//var Producer = kafka.Producer;
+//var KeyedMessage = kafka.KeyedMessage;
+//var Client = kafka.Client;
+//var client = new Client('172.16.4.92:9092');
+//var argv = require('optimist').argv;
+//var topic = argv.topic || 'topic1';
+//var p = argv.p || 0;
+//var a = argv.a || 0;
+//var producer = new Producer(client, {requireAcks: 1});
+//
+//producer.on('ready', function () {
+//    var message = 'a message';
+//    var keyedMessage = new KeyedMessage('keyed', 'a keyed message');
+//    console.log('start to send');
+//
+//    producer.send([
+//        {topic: topic, partition: p, messages: [message, keyedMessage], attributes: a}
+//    ], function (err, result) {
+//        console.log(err || result);
+//        process.exit();
+//    });
+//});
+//
+//producer.on('error', function (err) {
+//    console.log('error', err)
+//});
+
 server.get('/hello', respond);
 function respond(req, res, next) {
 
