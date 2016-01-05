b83aed4535eb3f70874ed6a7037aa27a49592fb1
Vance Zhao <zhaokai@nonobank.com>
2016-01-05 17:40:32 +0800

    no message

--- rpc/kafkaRedis.js
+++ rpc/kafkaRedis.js
@@ -12,7 +12,7 @@ var restify = require('restify');
 var server = restify.createServer();
 var kafka = require('kafka-node'),
     HighLevelProducer = kafka.HighLevelProducer,
-    client = new kafka.Client('172.16.4.92:2181,172.16.4.93:2181,172.16.4.94:2181'),
+    client = new kafka.Client('172.16.4.92:2181'),
     producer = new HighLevelProducer(client);
 
 producer.on('ready', function () {
@@ -46,10 +46,10 @@ function respond(req, res, next) {
     });
 
     //sync send msg
-    sendMsg(phone);
+    //sendMsg(phone);
 
     res.send('OK');
-    return next();
+    //return next();
 }
 
 function sendMsg(phone) {
